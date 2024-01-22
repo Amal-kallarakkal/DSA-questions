1ef3237155261390738af09f6adb1e7e4b251ace
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-01-22 20:46:25 +0530

    Time: 80 ms (12.09%) | Memory: 36.6 MB (8.31%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        vector<int> ans ;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int miss = 0;
+        int val = 1;
+        map<int,int> mp;
+
+        for(int i = 0 ; i < n  ; i++) {
+
+            mp[nums[i]]++;
+        }
+
+        for(int i = 1 ; i <= n ; i++) {
+            if(mp.find(i) == mp.end()) {
+                miss =  i;
+            }
+            if(mp[i] == 2) ans.push_back(i);
+        }
+
+        ans.push_back(miss);
+        return ans;
+        
+    }
+};
