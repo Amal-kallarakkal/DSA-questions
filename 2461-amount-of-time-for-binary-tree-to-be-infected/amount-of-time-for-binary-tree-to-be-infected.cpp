660fa41020dcc6d40e9286b071b4d1797add6482
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-02-07 20:14:28 +0530

    Time: 1268 ms (5.04%) | Memory: 169.2 MB (61.62%) - LeetSync

---
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -0,0 +1,89 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    TreeNode* Mapping(TreeNode* root, map<TreeNode*, bool> &visited, map<TreeNode*,TreeNode*> &parent, int start) {
+
+        if(root == NULL) return NULL;
+
+        visited[root] = false;
+        TreeNode* ans;
+        TreeNode* temp ;        
+        queue<TreeNode*> q;
+
+        parent[root] = NULL;
+        q.push(root);
+
+        while(!q.empty()) {
+            temp = q.front();
+            if(temp->val == start) ans = temp;
+            q.pop();
+            visited[temp] = false;
+
+            if(temp->left) {
+                q.push(temp->left);
+                parent[temp->left] = temp;
+            }
+
+            if(temp->right) {
+                q.push(temp->right);
+                parent[temp->right] = temp;
+            }
+
+        }
+
+        return ans;
+
+    }
+
+    int CalculateTime(TreeNode* root, map<TreeNode*, TreeNode*> &parent, map<TreeNode*,bool> &visited ) {
+        
+        if(root == NULL) {
+            return 0;
+        }
+        
+        visited[root] = true;
+        
+        int lH = 1;
+        int rH = 1;
+        int pH = 1;
+        
+        if(root->left && !visited[root->left]) {
+            lH += CalculateTime(root->left, parent, visited);
+        }
+        
+        if(root->right && !visited[root->right]) {
+            rH += CalculateTime(root->right, parent, visited);
+        }
+        
+        if(parent[root] != NULL && !visited[parent[root]] ) {
+            pH += CalculateTime(parent[root], parent, visited);
+        }
+        
+        return max(max(lH,rH),pH);
+    }
+
+    int amountOfTime(TreeNode* root, int start) {
+       int time = 0 ;
+       map<TreeNode*,TreeNode*> parent;
+       map<TreeNode*, bool> visited;
+       TreeNode* target;
+
+       target = Mapping(root, visited, parent, start);
+       
+       time = CalculateTime(target, parent, visited);
+
+       return time - 1;
+
+    }
+};
