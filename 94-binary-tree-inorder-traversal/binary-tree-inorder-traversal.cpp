6374aa0ef31ba86b6bff072835e3c3b1ddb29f39
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-02-09 00:32:14 +0530

    Time: 4 ms (33.30%) | Memory: 9.8 MB (25.14%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+
+        TreeNode* curr = root;
+        TreeNode* prev;
+
+        while (curr != NULL) {
+            if (curr->left == NULL) {
+                ans.push_back(curr->val);
+                curr = curr->right;
+            } else {
+                // go to the rightmost node (of the left subtree)
+                prev = curr->left;
+                while (prev->right != NULL && prev->right != curr) {
+                    prev = prev->right;
+                }
+
+                // check if prev-> right is NULL or not
+
+                if (prev->right == NULL) {
+                    prev->right = curr;
+                    // move the curr to its left
+                    curr = curr->left;
+                } else {
+                    // if prev->right == curr, push curr into ans(which means we
+                    // are inserting a root node) and cut the link between curr and prev.
+                    ans.push_back(curr->val);
+                    prev->right = NULL;
+
+                    // mving onto the Right- subtree/node
+                    curr = curr->right;
+                }
+            }
+        }
+        return ans;
+    }
+};
