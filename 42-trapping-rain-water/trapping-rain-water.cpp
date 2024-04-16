a4116682fd53357b5b174f05b7f9ae35c438e661
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-04-16 16:46:36 +0530

    Time: 15 ms (20.11%) | Memory: 22.3 MB (62.88%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -4,35 +4,34 @@ public:
         int n = height.size();
         int ans = 0;
         int prev_max = 0;
+        int res = 0;
         int P_ind = 0;
         for(int i = 0 ; i < n; i++) {
-            if(height[i] >= prev_max) {
-                int j = P_ind + 1;
-                while(j<i) {
-                    ans += prev_max - height[j];
-                    j++;
-                }
+            if(height[i] >= prev_max) {                
                 prev_max = height[i];
                 P_ind = i;
+                res += ans;
+                ans = 0;
             }
+            else ans += prev_max - height[i];
         }
 
-        cout<<ans<<endl;
+        
+        cout<<res<<endl;
         int Next_max = 0;
+        ans = 0;
         int N_ind = n-1;
         for(int i = n - 1; i >= P_ind; i--) {
             if(height[i] >= Next_max) {
-                int j = N_ind - 1;
-                while(j>i) {
-                    ans += Next_max - height[j];
-                    j--;
-                }
                 Next_max = height[i];
                 N_ind = i;
-            }    
+                res += ans;
+                ans = 0;
+            } 
+            else ans += Next_max - height[i];   
         }
 
-         cout<<ans<<endl;
-        return ans;
+        //  cout<<ans<<endl;
+        return res;
     }
 };
