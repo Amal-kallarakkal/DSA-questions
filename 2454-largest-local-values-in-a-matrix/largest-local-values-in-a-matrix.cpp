c5f0a425534eb3c41da91d8a84c39aa48bf4cce5
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-05-12 23:53:28 +0530

    Time: 10 ms (61.02%) | Memory: 13.7 MB (33.59%) - LeetSync

--- 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.cpp
+++ 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
         ios::sync_with_stdio(0);
-        int rows = grid.size() - 2;
-        int column = grid[0].size() - 2;
-        vector<vector<int>> res(rows, vector<int>(column));
+        int n = grid.size() - 2;
+        vector<vector<int>> res(n, vector<int>(n));
 
-        for(int i = 0 ; i < rows ; i++) {
-            for(int j = 0 ; j < column; j++) {
+        for(int i = 0 ; i < n ; i++) {
+            for(int j = 0 ; j < n; j++) {
                 int maxi = grid[i][j];
                 for(int a = i; a< i+3; a++) {
                     
