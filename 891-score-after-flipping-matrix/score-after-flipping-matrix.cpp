4de3da71cf5f8392abbd325ea27aa751ba1eb553
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-05-13 21:13:41 +0530

    Time: 2 ms (54.61%) | Memory: 10.3 MB (37.53%) - LeetSync

---
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int matrixScore(vector<vector<int>>& grid) {
+
+        int row = grid.size();
+        int col = grid[0].size();
+        // row flip
+        for(int i = 0 ; i< row; i++) { 
+            if(grid[i][0] == 0 ) {
+                for(int j = 0 ; j< col; j++) grid[i][j] = !grid[i][j];
+            }
+        }
+
+        // col flip
+        for(int j = 0 ; j < col; j++){
+            int cnt1 = 0;
+            for(int i = 0; i < row; i++) {
+                if(grid[i][j] == 1) cnt1++;
+            }
+            if(cnt1 <= row/2) {
+                cout<<"cnt1 :"<<cnt1<<endl;
+                for(int i = 0; i < row; i++) grid[i][j] = !grid[i][j];
+            }
+        }
+        int sum = 0;
+         for(int i = 0 ; i< row; i++) {
+             for(int j = 0 ; j< col; j++) sum += grid[i][j]*pow(2,col-1 -j);
+             cout<<"sum: "<<sum<<endl;
+         }         
+               
+        return sum;
+    }
+};
