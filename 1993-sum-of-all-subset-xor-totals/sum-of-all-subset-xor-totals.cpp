f1d356412683ada8c73b070bb640a08dc2edeccd
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-05-20 09:20:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (65.34%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -2,24 +2,11 @@ class Solution {
 public:
 
     int solve(vector<int>& nums,  int i, int Xor) {
-
         if(i >= nums.size()) return Xor;
-
-        // excluding         
-        int exc = solve(nums, i + 1, Xor);
-       
-        // including        
-        Xor = Xor xor nums[i];        
-        int inc = solve(nums, i + 1, Xor);
-        
-        return inc + exc;
+        return solve(nums, i + 1, Xor xor nums[i]) + solve(nums, i + 1, Xor);
     }
     int subsetXORSum(vector<int>& nums) {
-        ios::sync_with_stdio(0);
-        int i = 0;
-        int Xor = 0;
-        int Xor_sum = solve(nums,i, Xor);
-      
-        return Xor_sum;
+        ios::sync_with_stdio(0);    
+        return solve(nums, 0, 0);;
     }
 };
