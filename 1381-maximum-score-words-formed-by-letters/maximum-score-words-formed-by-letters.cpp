d0ab740710a299274f97396bdc85b9b500b2df59
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-05-24 17:40:37 +0530

    Time: 18 ms (38.84%) | Memory: 13.3 MB (79.03%) - LeetSync

--- 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.cpp
+++ 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int solve(int idx, vector<string>& words, map<char, int> &mp,  map<char, int> &ltr, int &ans){
+    int solve(int idx, vector<string>& words, vector<int>& score,  map<char, int> &ltr, int &ans){
 
         if(idx >= words.size()) return ans;
 
         // icluding the current string
-        int score = 0;
+        int scores = 0;
         string incomp = "";  
        
         for(int i = 0; i <words[idx].size(); i++ ) {
             if(ltr[words[idx][i]] != 0) {
                 incomp += words[idx][i];
-                score += mp[words[idx][i]];
+                scores += score[words[idx][i] - 97];
                 ltr[words[idx][i]]--;
             } else {
-                score = 0;
+                scores = 0;
                 for(int j = 0 ; j < incomp.size(); j++) {
                     ltr[incomp[j]]++;
                 }
@@ -23,16 +23,16 @@ public:
             }
         }
 
-        ans += score;
-        int inc = solve(idx + 1, words, mp, ltr, ans);
+        ans += scores;
+        int inc = solve(idx + 1, words, score, ltr, ans);
 
         // backtrack
         for(int j = 0 ; j < incomp.size(); j++) {
             ltr[incomp[j]]++;
         }
-        ans -= score;
+        ans -= scores;
         
-        int exc = solve(idx + 1, words, mp, ltr, ans);
+        int exc = solve(idx + 1, words, score, ltr, ans);
 
         return  max(inc , exc);
     }
@@ -41,20 +41,14 @@ public:
 
         ios::sync_with_stdio(0);
 
-        map<char, int> mp;
         map<char, int> ltr;
         
-        // score map
-        char a = 'a';
-        for (int i = 0; i < 26; i++) {
-            mp[a++] = score[i];
-        }
         // limited letters count map
         for (int i = 0; i < letters.size(); i++) {
             ltr[letters[i]]++;
         }
         int ans = 0;
-        int finans = solve(0, words, mp, ltr, ans);
+        int finans = solve(0, words, score, ltr, ans);
 
         
         return finans;
