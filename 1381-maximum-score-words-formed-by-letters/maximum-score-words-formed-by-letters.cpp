8ad74ecfee2829b5f710ca77a462fd2729e1df4c
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-05-24 17:21:33 +0530

    Time: 26 ms (24.08%) | Memory: 13.5 MB (78.45%) - LeetSync

--- 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.cpp
+++ 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.cpp
@@ -6,12 +6,8 @@ public:
 
         // icluding the current string
         int score = 0;
-        string incomp = "";        
+        string incomp = "";  
        
-        // // cout<<"before backtracking for "<<words[idx]<<" : "<<endl;
-
-        // for(auto x : ltr) cout<<x.first<<" : "<<x.second<<" ";
-
         for(int i = 0; i <words[idx].size(); i++ ) {
             if(ltr[words[idx][i]] != 0) {
                 incomp += words[idx][i];
@@ -22,36 +18,34 @@ public:
                 for(int j = 0 ; j < incomp.size(); j++) {
                     ltr[incomp[j]]++;
                 }
-                incomp = "";
-                // cout<<words[idx]<<" cant be formed with given letters"<<endl;
+                incomp = "";               
                 break;
             }
         }
 
         ans += score;
-        // cout<<"score for "<<words[idx]<<": "<<ans<<endl;
         int inc = solve(idx + 1, words, mp, ltr, ans);
 
-        // cout<<"include "<<words[idx]<<" : "<<inc<<endl;
         // backtrack
         for(int j = 0 ; j < incomp.size(); j++) {
             ltr[incomp[j]]++;
         }
         ans -= score;
         
-
         int exc = solve(idx + 1, words, mp, ltr, ans);
-        // cout<<"exclude "<<words[idx]<<" : "<<exc<<endl;
 
         return  max(inc , exc);
     }
 
     int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
-        int maxi = 0;
+
+        ios::sync_with_stdio(0);
+
         map<char, int> mp;
         map<char, int> ltr;
-        char a = 'a';
+        
         // score map
+        char a = 'a';
         for (int i = 0; i < 26; i++) {
             mp[a++] = score[i];
         }
