fc77f6cf54588565620db1a03582f555237b6ceb
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-05-27 10:46:25 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (53.24%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
-
-    int binarySearch(vector<int>& nums, int low, int high) {
-        if(low > high) {
-            return -1;
-        }
-
-        int mid = (high + low)/2;
-
-        int cnt = 0;
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] >= mid) {
-                // cnt = nums.size() - i;
-                // break;
-                cnt++;
-            }
-        }
-        if(cnt == mid) return mid;
-        else if(cnt > mid) return binarySearch(nums, mid + 1, high);
-        else return binarySearch(nums,low, high - 1);
-
-        // return -1;
-    }
-
     int specialArray(vector<int>& nums) {
-        ios::sync_with_stdio(0);
-        int high = 0;
-        for(int i = 0 ; i < nums.size(); i++) high = max(high, nums[i]);
-        // sort(nums.begin(), nums.end());
-        return binarySearch(nums,0,high);
+        
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<=1000;i++){
+            int s=0,e=nums.size()-1,ans=-1;
+            while(s<=e){
+                int mid=s+(e-s)/2;
+                if(nums[mid]>=i){
+                    ans=mid;
+                    e=mid-1;
+                }
+                else s=mid+1;
+            }
+            if(nums.size()-ans==i && ans!=-1 ) return i;
+        }
+        return -1;
     }
 };
