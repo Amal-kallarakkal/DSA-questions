8005536d8d2445a50c4a20b4637148c6f3fcc330
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-05-28 17:36:35 +0530

    Time: 7 ms (49.05%) | Memory: 9.6 MB (7.76%) - LeetSync

--- 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.cpp
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.cpp
@@ -3,11 +3,12 @@ public:
     int equalSubstring(string s, string t, int maxCost) {
         ios::sync_with_stdio(0);
         int n = s.size();
-        vector<int> arr(n);
+        vector<int> arr(n,0);
 
         for(int i = 0; i < n; i++) {
             arr[i] = abs(s[i] - t[i]);            
         }
+
         int st = 0;
         int e = st;
         int maxCnt = 0;
@@ -15,7 +16,7 @@ public:
         while (e < n) {
             sum += arr[e];
 
-            while (sum > maxCost) {
+            while (st <= e && sum > maxCost) {
                 sum -= arr[st];
                 st++;
             }
