f9d647b863a592fdc8810a8018d843851d8aa4ea
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-05-29 19:27:38 +0530

    Time: 4 ms (25.58%) | Memory: 7.6 MB (47.06%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -9,10 +9,12 @@ public:
         
         while(s.size() > 1) {
             i = s.size() - 1;
-
+            // if s is even pop the rightmost element(right shift or simply divide by 2)
             if(s[i] == '0') {                
                 s.pop_back();         
-            } else {
+            } 
+            
+            else {
                 int j = i;
                 while(j >= 1 && s[j] != '0') {
                     if(s[j] == '1'){
@@ -20,10 +22,12 @@ public:
                     }                     
                     j--;
                 }
+                // if odd we flip the rightmost "0" to "1" 
                 if(j>=1 && s[j] == '0') {
-                    s[j] = '1';
-                    
-                } else {
+                    s[j] = '1';                    
+                } 
+                // if no "0"s are present then we add '0' to string s
+                else {
                     s += '0';                   
                 }                     
             }
