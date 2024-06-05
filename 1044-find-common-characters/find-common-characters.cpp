1a4377b97626c238dcd3b221fd4d5bb218004523
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-06-05 14:09:55 +0530

    Time: 9 ms (50.60%) | Memory: 11.9 MB (41.98%) - LeetSync

---
+++ 1044-find-common-characters/find-common-characters.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        sort(words.begin(), words.end());
+
+        for(int i = 0 ; i < words.size(); i++) {
+            sort(words[i].begin(), words[i].end());
+        }
+
+        // for(int i = 0 ; i < words.size(); i++) {
+        //     cout<<words[i]<<" ";
+        // }
+        
+        string res = words[0];
+
+        for(int i = 1 ; i < words.size(); i++) {
+            int ri = 0;
+            int j = 0;
+            string update ="";
+
+            while(j < words[i].size() && ri < res.size() ) {
+                // cout<<words[i][j]<<" "<<res[ri]<<endl;
+                if(words[i][j] == res[ri]) {
+                    // cout<<res[ri]<<endl;
+                    update += res[ri];
+                    ri++;
+                    j++;
+                } else if(words[i][j] > res[ri]) {
+                    ri++;
+                } else j++;
+                // cout<<update<<endl;
+                
+            }
+            res = update;
+        }
+        vector<string> ans(res.size());
+        for(int  i = 0 ; i < res.size(); i++) {
+            ans[i] = res[i];
+        }
+
+        return ans;
+    }
+};
