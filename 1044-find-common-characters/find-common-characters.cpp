ff292ee0e13b9c24cb7065a351029f6e594a5e41
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-06-05 14:21:10 +0530

    Time: 6 ms (70.90%) | Memory: 11.5 MB (60.89%) - LeetSync

--- 1044-find-common-characters/find-common-characters.cpp
+++ 1044-find-common-characters/find-common-characters.cpp
@@ -1,41 +1,22 @@
 class Solution {
 public:
     vector<string> commonChars(vector<string>& words) {
-        ios::sync_with_stdio(0);
-
-        sort(words.begin(), words.end());
-
-        for(int i = 0 ; i < words.size(); i++) {
-            sort(words[i].begin(), words[i].end());
-        }
-
-        string res = words[0];
-
-        for(int i = 1 ; i < words.size(); i++) {
-            int ri = 0;
-            int j = 0;
-            string update ="";
-
-            while(j < words[i].size() && ri < res.size() ) {
-                // cout<<words[i][j]<<" "<<res[ri]<<endl;
-                if(words[i][j] == res[ri]) {
-                    // cout<<res[ri]<<endl;
-                    update += res[ri];
-                    ri++;
-                    j++;
-                } else if(words[i][j] > res[ri]) {
-                    ri++;
-                } else j++;
-                // cout<<update<<endl;
-                
+        vector<int> count(26, INT_MAX);
+        vector<string> res;
+        for (const auto& w : words) {
+            vector<int> counts(26, 0);
+            for (const auto& c : w) {
+                counts[c - 'a']++;
             }
-            res = update;
+            for (int i = 0; i < 26; i++) {
+                count[i] = min(count[i], counts[i]);
+            }
         }
-        vector<string> ans(res.size());
-        for(int  i = 0 ; i < res.size(); i++) {
-            ans[i] = res[i];
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < count[i]; j++) {
+                res.push_back(string(1, 'a' + i));
+            }
         }
-
-        return ans;
+        return res;
     }
 };
