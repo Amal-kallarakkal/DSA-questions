2d33941aa07bbb606c4d5e837b01f5610cb42c96
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-06-07 16:57:22 +0530

    Time: 203 ms (34.07%) | Memory: 41.9 MB (88.02%) - LeetSync

---
+++ 648-replace-words/replace-words.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        string ans = "";
+
+        int start = 0;
+        int end = start + 1;
+        unordered_map<string, int> mp;
+        for(int i = 0 ; i < dictionary.size(); i++) mp[dictionary[i]]++;
+
+        string t = "";
+        string tt = "";
+        while(start < sentence.size()) {
+            end = start +1;
+            while(end < sentence.size() && sentence[end] != ' ' ) {
+                end++;
+            }
+            t = sentence.substr(start, end - start);
+            for(int j = 0 ; j < t.size(); j++ ) {
+                tt += t[j];
+                if(mp.count(tt)) {
+                    break;
+                }
+            }
+            if(tt == "" ) {
+                if(ans == "")  ans += t;
+                else ans += " " + t;
+            }    
+            else {
+                if(ans == "")  ans += tt;
+                else ans += " " + tt;
+            }    
+            tt = "";
+            start = end + 1;
+        }
+
+        return ans;
+    }
+};
