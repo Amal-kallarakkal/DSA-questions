59cceb8902dfaa2a57c0f3d85e878d4b08841a61
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-06-10 17:41:44 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (44.04%) - LeetSync

--- 1137-height-checker/height-checker.cpp
+++ 1137-height-checker/height-checker.cpp
@@ -5,12 +5,12 @@ public:
         ios::sync_with_stdio(0);
         int cnt = 0;
         int n = heights.size();
-        vector<int> nums(n);
+        int nums[n];
         for(int i = 0 ; i < heights.size(); i++) {
             nums[i] = heights[i];
         }
 
-        sort(nums.begin(), nums.end());
+        sort(heights.begin(), heights.end());
 
         for(int i = 0; i < n ; i++) {
             if( nums[i] != heights[i]) cnt++;
