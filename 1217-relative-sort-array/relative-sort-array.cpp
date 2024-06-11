92cbc5741bdfc382df7a4b928cdabb1ba13876cd
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-06-11 14:02:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (13.68%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -3,32 +3,30 @@ class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
         ios::sync_with_stdio(0);
-        unordered_map<int,int> mp;
-        unordered_map<int,int> mp2;
-        vector<int> ans(arr1.size());
-        for(int i = 0 ; i < arr2.size(); i++) {
-            mp[arr2[i]]  = i + 1;           
-        }
-        int end = arr1.size() - 1;
+        map<int,int> mp2;
+        vector<int> ans;
+        int a, b, t;
+
         for(int i = 0; i < arr1.size(); i++) {
-            if(mp[arr1[i]]) mp2[arr1[i]]++;  
-            else ans[end--] = arr1[i];
-                
+            mp2[arr1[i]]++;              
         }
-        int k = 0;
+
         for(int i = 0 ; i < arr2.size(); i++) {
-            int t = mp2[arr2[i]];
+            t = mp2[arr2[i]];
             while(t--) {
-                ans[k] = arr2[i];
-                k++;
+                ans.push_back(arr2[i]);
+                mp2[arr2[i]]--;
+                if(!mp2[arr2[i]]) mp2.erase(arr2[i]);
             }
         }
-
-        sort(ans.begin() + end + 1, ans.end());
-
+        
+        for(auto x: mp2) {
+            a = x.first;
+            b = x.second;
+            while(b--) ans.push_back(a);
+        }
 
         return ans;
-
 
     }
 };
