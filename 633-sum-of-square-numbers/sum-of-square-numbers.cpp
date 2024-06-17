91a8755419a091eb03094bf24f25da2236b6d155
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-06-17 12:48:17 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (61.35%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -2,17 +2,16 @@ using ll = long long;
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-        int root = sqrt(c);
-        ll low = 0 , high = root;
-        // bool ans = false;
+        
+        ll low = 0 , high = sqrt(c);   
         ll sum = 0;
        
-        while(low <= root && low <= high) {
+        while(low <= high) {
             sum = low*low + high*high;
             if(sum > c) high--;
             else if(sum == c) return true;
             else low++;   
-            // cout<<sum<<endl;            
+           
         }           
         
 
