d6610691d00e3b550f83249b1d49d29dd6b7ae14
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-06-27 12:01:00 +0530

    Time: 109 ms (97.82%) | Memory: 70.5 MB (99.34%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,13 +1,13 @@
+#pragma GCC optimize("OFast")
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        int centre = 0;
-        
-        if(edges[0][0] == edges[1][0]) centre = edges[0][0];
-        else if(edges[0][0] == edges[1][1]) centre = edges[0][0];
-        else centre = edges[0][1];  
-   
+        ios::sync_with_stdio(0);
+              
+        if(edges[0][0] == edges[1][0]) return edges[0][0];
+        else if(edges[0][0] == edges[1][1]) return edges[0][0];
+        else return edges[0][1];  
 
-        return centre;
+        return -1;
     }
 };
