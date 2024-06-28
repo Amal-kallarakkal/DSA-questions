6f3ace025e3dd6e10de98263d688d980f762fe8a
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-06-28 10:25:55 +0530

    Time: 284 ms (67.59%) | Memory: 134.5 MB (48.10%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
@@ -1,10 +1,13 @@
+#pragma GCC optimize("OFast")
 using ll = long long;
 class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
+        ios::sync_with_stdio(0);
         unordered_map<int,int> mp;
         ll sum = 0;
         priority_queue<pair<int,int>> pq;
+
         for(int i = 0 ; i < roads.size(); i++) {
             mp[roads[i][0]]++;
             mp[roads[i][1]]++;
@@ -18,9 +21,9 @@ public:
             x = pq.top();
             pq.pop();
             mp[x.second] = n--;
-            // cout<<x.second<<" : "<<x.first<<endl; 
+            
         }
-        
+
         for(int i = 0 ; i < roads.size(); i++) {
            sum += mp[roads[i][0]] + mp[roads[i][1]] ;
         }
