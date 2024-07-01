f571e6636b2f289392d6a37297c5ed04c0740bcf
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-07-01 10:56:04 +0530

    Time: 0 ms (100%), Space: 11.1 MB (34.92%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,14 +1,17 @@
+#pragma GCC optimize("OFast")
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+        ios::sync_with_stdio(0);
+        cin.tie(NULL);
+        cout.tie(NULL);
         string ans = strs[0];
-        // sort(strs.begin(), strs.end());
+        
         string temp = "";
         for(int i = 1 ; i < strs.size(); i++) {
             temp = "";
             for(int j = 0; j < strs[i].size(); j++) {
-                if(ans[j] != strs[i][j]) break;
-                
+                if(ans[j] != strs[i][j]) break;                
                 temp += ans[j];
             }
             ans = temp;
