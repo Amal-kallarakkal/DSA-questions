540575e1377544c9478048e4488aed43acf4f436
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-07-02 15:16:00 +0530

    Time: 13 ms (30.67%) | Memory: 9.5 MB (18.35%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,36 +1,21 @@
-#pragma GCC optimize("OFast")
 class Solution {
 public:
-    
-    int solve(string &s, int &k, char &C, int &maxi) {
-        int cnt = 0;
-        
-        // for maximum continous char C(any unique char in s)
-        int j = 0;
-        for(int i = 0 ; i < s.size() - maxi; i++) {
-            while(j <  s.size() && cnt <= k ) {
-                if(s[j] != C) cnt++;
-                if(cnt > k) {
-                    cnt--;
-                    
-                    break;
-                }
-                j++;
-            }
-            
-            maxi = max(maxi, j - i);            
-            if(s[i] != C) cnt--;       
-            
-        }
-        return maxi;
-    }
     int characterReplacement(string s, int k) {
-        unordered_set<char> set;
-        for(int i = 0 ; i < s.size(); i++) set.insert(s[i]);
-        int maxi = 0;
-        for(auto x: set) {
-            maxi = max(maxi, solve(s, k, x, maxi));
+        int n = s.size();
+        int i = 0, j = 0, maxi = 0;
+        unordered_map<char,int>mp;
+        int ans = -1;
+        while(j < n)
+        {
+            mp[s[j]]++;
+            maxi = max(maxi, mp[s[j]]);
+            if((j-i+1) - maxi > k){
+                mp[s[i]]--;
+                i++;
+            }
+            ans = max(ans, (j-i+1));
+            j++;   
         }
-        return maxi;
+        return ans;
     }
 };
