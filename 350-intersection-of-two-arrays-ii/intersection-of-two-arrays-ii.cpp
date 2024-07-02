af6594a7bfa45791789840c1cc9b6199f50f9895
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-07-02 14:37:04 +0530

    Time: 10 ms (6.27%) | Memory: 14.3 MB (15.24%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -4,26 +4,32 @@ public:
     // ios::sync_with_stdio(false);
     // cin.tie(NULL);
     // cout.tie(NULL);
-    vector<int> maping(vector<int> &nums1, vector<int> &nums2) {
+    // vector<int> maping(vector<int> &nums1, vector<int> &nums2) {
+    //     vector<int> ans;
+    //     unordered_map<int, int> mp;
+
+    //     for(int i = 0; i < nums1.size(); i++) mp[nums1[i]]++;
+    //     for(int i = 0; i < nums2.size(); i++) {
+    //         if(mp.count(nums2[i])) {
+    //             ans.push_back(nums2[i]);
+    //             mp[nums2[i]]--;
+    //             if(mp[nums2[i]] == 0) mp.erase(nums2[i]);
+    //         }
+    //     }
+    //     return ans;
+    // }
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        
         vector<int> ans;
         unordered_map<int, int> mp;
 
         for(int i = 0; i < nums1.size(); i++) mp[nums1[i]]++;
         for(int i = 0; i < nums2.size(); i++) {
-            if(mp.count(nums2[i])) {
+            if(mp[nums2[i]] > 0) {
                 ans.push_back(nums2[i]);
                 mp[nums2[i]]--;
-                if(mp[nums2[i]] == 0) mp.erase(nums2[i]);
             }
         }
         return ans;
-    }
-    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-       
-        return n1>n2 ? maping(nums1, nums2): maping(nums2, nums1);
-        
     }
 };
