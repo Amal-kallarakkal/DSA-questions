76d26f0f9e1e27cda3c6da9ee7936a2486f5ebbf
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-07-07 13:36:57 +0530

    Time: 3 ms (13.78%) | Memory: 7.2 MB (30.43%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -3,14 +3,7 @@ class Solution {
 public:
     // ios::sync_with_stdio(0);
     int numWaterBottles(int numBottles, int numExchange) {
-        int empty = 0, sum = 0 ;
         
-        while(numBottles) {
-            sum += numBottles;
-            empty += numBottles;
-            numBottles = empty/numExchange;
-            empty = empty%numExchange;            
-        }
-        return sum;
+        return (numBottles*numExchange-1)/(numExchange-1);
     }
 };
