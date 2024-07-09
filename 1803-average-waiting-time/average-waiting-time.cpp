fd2da8508ced618256f185809b258a4bc66e08b1
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-07-09 20:11:20 +0530

    Time: 151 ms (55.88%) | Memory: 77.6 MB (20.88%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -4,8 +4,7 @@ public:
         int n = nums.size();
         vector<int> wait(n, 0);
         int time = 0;
-        // wait[0] = nums[0][1];
-        int w = 0;
+        
         for(int i = 0; i < n ; i++) {
             
             if(time <= nums[i][0] ){
@@ -13,13 +12,9 @@ public:
             }  else {
                 time += nums[i][1];
             }
-                w = time - nums[i][0];
-                // cout<<time<<" - "<<nums[i][0]<<" = "<<w<<endl;          
-
-            wait[i] = w;
+            wait[i] = time - nums[i][0];
         }
         long long int sum = 0;
-        double avg = (accumulate(wait.begin(), wait.end(), sum))*1.0/n;
-        return avg;
+        return (accumulate(wait.begin(), wait.end(), sum))*1.0/n;
     }
 };
