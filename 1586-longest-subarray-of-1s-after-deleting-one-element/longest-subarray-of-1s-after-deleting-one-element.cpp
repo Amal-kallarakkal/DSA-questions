b0779824ae7a390ef6edcb8d1ebd72b913a664d0
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-07-11 19:45:29 +0530

    Time: 39 ms (72.91%) | Memory: 58.9 MB (9.10%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -5,15 +5,11 @@ public:
         int cnt = 0, i = 0 , j = 0, n = nums.size();
 
         while(i < n) {
-           cout<<i<<" , "<<j<<" , "<<cnt<<endl;
            while(cnt <= 1 && j < n) {
-                if(!nums[j]) cnt++;
-                // if(cnt > 1) break;
+                if(!nums[j]) cnt++;                
                 j++;
            }
            maxi = (cnt)? max(maxi, j - i - cnt): max(maxi, j - i - 1);
-
-        //    j++;
            if(!nums[i]) cnt--;
            i++;
         }
