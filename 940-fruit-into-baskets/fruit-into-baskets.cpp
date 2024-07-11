662e81c99f336fb08ebe12292fc915ace3d476dc
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-07-11 15:46:37 +0530

    Time: 785 ms (5.06%) | Memory: 128.3 MB (10.84%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        unordered_set<int> set;
+        int i = 0, j = 0, temp, ti;
+        int n = fruits.size();
+        int maxi = 0;
+        while(i < n - maxi) {
+            j = i;
+            while(j < n) {
+                set.insert(fruits[j]);
+                if(set.size() >= 3) {
+                    cout<<"break at j: "<<j<<endl;
+                    break;
+                }   
+                j++;
+            }
+            maxi = max(maxi, j - i);
+            // cout<<i<<" size: "<<set.size()<<" j: "<<j<<endl;
+            set.clear();
+            i++;
+            cout<<i<<endl;
+        }
+        return maxi;
+    }
+};
