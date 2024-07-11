7e57327755771e9a1b0485a1da716cb4e1606cf7
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-07-11 19:48:06 +0530

    Time: 27 ms (98.40%) | Memory: 58.9 MB (9.22%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,4 +1,10 @@
-#pragma GCC optimize("OFast")
+#pragma GCC optimize("O3")
+#pragma GCC target("avx2, bmi, bmi2, lzcnt, popcnt")
+static const bool __boost = [](){
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return ios_base::sync_with_stdio(false);
+}();
 class Solution {
 public:
     // ios::sync_with_stdio(0);
