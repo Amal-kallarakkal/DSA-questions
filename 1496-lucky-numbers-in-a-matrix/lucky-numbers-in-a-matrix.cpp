e7a2a15666927cfe553cddf2ac6c1d5ed8818042
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-07-19 14:28:29 +0530

    Time: 17 ms (39.49%) | Memory: 15.3 MB (5.19%) - LeetSync

---
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> luckyNumbers (vector<vector<int>>& matrix) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        unordered_set<int> rset;
+        unordered_set<int> cset;
+        unordered_set<int> set;
+        vector<int> ans;
+        int maxi, mini;
+
+        for(int i = 0 ; i < row; i++) {
+            mini = INT_MAX;
+            for(int j = 0; j < col; j++) {
+                mini = min(matrix[i][j] , mini);
+            }
+            // cout<<maxi<<endl;
+            rset.insert(mini);
+        }
+
+        for(int i = 0 ; i < col; i++) {
+            maxi = 0;
+            for(int j = 0; j < row; j++) {
+                maxi = max(matrix[j][i] , maxi);
+            }
+            // cout<<maxi<<endl;
+            cset.insert(maxi);
+        }
+
+        for(auto x: rset){
+            if(cset.count(x)) set.insert(x);
+        }
+        
+        for(auto x: cset){
+            if(rset.count(x)) set.insert(x);
+        }
+        for(auto x: set){
+            ans.push_back(x);
+        }
+
+        return ans;
+
+    }
+};
