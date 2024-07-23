b897089653120e3d6aa354103ada57c4f076ee33
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-07-23 18:05:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (6.82%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,27 +1,8 @@
 class Solution {
 public:
 
-    long long int fact(long long int x){
-        //base case
-        if(x == 1 || x == 0){
-            return 1;
-        }
-
-        return x*fact(x-1);
-    }
-
-    long long int comb(int n, int i) {
-
-        long long int ans = fact(n)/fact(i);
-        long long int ans2 = ans / fact(n-i);
-        return ans2;
-    }
     vector<int> getRow(int rowIndex) {
         vector<vector<int>> nums(rowIndex + 1);
-        // nums[0][0] = 1;
-        // nums[1].push_back(1);
-        // nums[1].push_back(1);
-
         int n = rowIndex;
         for(int i = 0 ; i <= n ; i++) {
             for(int j = 0 ; j < i+1 ; j++ ) {
@@ -33,16 +14,9 @@ public:
                 }               
             }            
         }
-        int k = 0;
-        vector<int> ans;
-        for(auto x: nums) {
-            k++;
-            if(k == n+1)for(auto y : x) ans.push_back(y);
-            
-        }
-        
-        return ans;
-        // return nums[n - 1];
+    
+        return nums[n];
+       
     }
     
 };
