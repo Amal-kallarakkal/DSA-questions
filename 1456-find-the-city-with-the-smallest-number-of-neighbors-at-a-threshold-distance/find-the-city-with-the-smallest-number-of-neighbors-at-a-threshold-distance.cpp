2ae916ebc812c0d8fd4c15ed8df5d04f024db855
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-07-27 01:40:37 +0530

    Time: 18 ms (96.85%) | Memory: 15.6 MB (75.72%) - LeetSync

---
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> dis(n, vector(n, 10001));
+        int res = 0, smallest = n;
+        for (auto& e : edges)
+            dis[e[0]][e[1]] = dis[e[1]][e[0]] = e[2];
+        for (int i = 0; i < n; ++i)
+            dis[i][i] = 0;
+        for (int k = 0; k < n; ++k)
+            for (int i = 0; i < n; ++i)
+                for (int j = 0; j < n; ++j)
+                    dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j]);
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; ++j)
+                if (dis[i][j] <= distanceThreshold)
+                    ++count;
+            if (count <= smallest) {
+                res = i;
+                smallest = count;
+            }
+        }
+        return res;
+    }
+    // int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+    //     vector<vector<int>> adj(n, vector<int>(n, 0));
+    //     int u, v, w;
+    //     for(int i = 0 ; i < n ; i++) {
+    //         u = edges[i][0];
+    //         v = edges[i][1];
+    //         w = edges[i][2];
+    //         adj[u][v] = w;
+    //         adj[v][u] = w;
+    //     }
+    //     int i = 0;
+    //     for(auto x: adj) {
+           
+    //         for(int y : x ) cout<<y<<" ";
+    //         cout<<endl;
+    //     }
+    //     int cost = 0; 
+    //     bfs()
+
+    //     return 0;
+    // }
+};
