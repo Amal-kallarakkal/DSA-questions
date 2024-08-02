0f9d6bb86ea816ee4f61378ecd6b32f1e36e39db
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-08-02 21:05:58 +0530

    Time: 54 ms (97.05%) | Memory: 91.6 MB (11.20%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -6,18 +6,23 @@ static const int fast = []{
 
     return 0;
 }();
+
 class Solution {
 public:
     int minSwaps(vector<int>& a) {
         int win = 0, n = a.size();
+
+        // count the total num of 1s , thats will be the window size => win.
         for(int i = 0 ; i < a.size(); i++) if(a[i]) win++;
 
+        // CREATE a new vector to push the first "win" number of elements at the back to create a cicular array
         vector<int> nums;
         nums.insert(nums.end(), a.begin(), a.end());        
         for(int i = 0; i< win; i++ ) nums.push_back(a[i]);
 
         int mini = INT_MAX;
         int cnt = 0;
+        // find the mini for first "win" elements
         for(int i = 0 ; i < win; i++) {
             if(nums[i] == 0) cnt++;
         }
@@ -26,6 +31,7 @@ public:
         int j;
         n = nums.size();
 
+        // basic sliding window 
         for(int i = 0 ; i < n - win; i++) {
             j = i + win;
             if(nums[i] == 0) cnt--;
