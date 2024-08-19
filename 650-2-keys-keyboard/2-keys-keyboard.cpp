b488fbca6ff128cc6f58e44cee805c7d854cd4a8
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-08-19 12:44:32 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (76.32%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    int minSteps(int n) {
-        if(n == 1) return 0;
+    int minSteps(int n) {        
         int ans = 0;
         int i = 2;
         while(i < n) {
