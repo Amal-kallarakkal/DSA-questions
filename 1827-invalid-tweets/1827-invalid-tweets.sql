c391dc1f149fab497e87977b70c816f66340d923
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-08-21 13:23:59 +0530

    Time: 530 ms (78.4%), Space: 0B (100%) - LeetHub

--- 1827-invalid-tweets/1827-invalid-tweets.sql
+++ 1827-invalid-tweets/1827-invalid-tweets.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 -- SELECT tweet_id FROM Tweets WHERE LEN(tweet_id) > 15;
 
-SELECT tweet_id FROM Tweets WHERE CHARACTER_LENGTH(content) > 15;
+SELECT tweet_id FROM Tweets WHERE LENGTH(content) > 15;
