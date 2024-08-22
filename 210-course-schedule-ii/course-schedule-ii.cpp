16c9ead3cdcf4f56e8565b5a7addf177b76b010f
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-08-23 00:44:18 +0530

    Time: 15 ms (67.75%) | Memory: 19.1 MB (11.94%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,37 +1,42 @@
 class Solution {
 public:
-    void dfs(int i, vector<int> &visited, vector<unordered_set<int>> &adj, vector<int> &ans) {
-        visited[i] = 1;
-
-        for(int j : adj[i]) {
-            if(!visited[j]) {
-                dfs(j, visited, adj, ans);
-            }
-        }
-        ans.push_back(i);
-    }
+    
     vector<int> findOrder(int numCourses, vector<vector<int>>& nums) {
         int v = numCourses;
         vector<unordered_set<int>> adj(v);
-        vector<int> visited(v, 0);
+        vector<int> indegree(v, 0);
+        queue<int> q;
         vector<int> ans;
 
         for(int i = 0 ; i < nums.size(); i++) {
             adj[nums[i][1]].insert(nums[i][0]);
         }
 
-        for(int i = 0 ; i < visited.size(); i++) {
-            if(!visited[i]) {
-                dfs(i, visited, adj, ans);
-            }            
+        for(int i = 0 ; i < v; i++) {
+            for(int j: adj[i]) {
+                indegree[j]++;
+            }
         }
        
         for(int i = 0; i < v ; i++) {
-            for(int j = i ; j < v; j++) {
-                if(adj[ans[i]].count(ans[j])) return {};
+            if(!indegree[i]) {
+                q.push(i);
             }
         }
-        reverse(ans.begin(), ans.end());
-        return ans;
+
+        int f ;
+        while(!q.empty()) {
+            f = q.front();
+            ans.push_back(f);
+            q.pop();
+
+            for(int i: adj[f]) {
+                indegree[i]--;
+                if(!indegree[i]) q.push(i);
+            }
+        }
+        if(ans.size() == v) return ans;
+
+        return {};
     }
 };
