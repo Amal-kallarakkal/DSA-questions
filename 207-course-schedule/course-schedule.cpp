92199a14ec0f6eea1e98f25c18d504a86197a736
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-08-22 20:46:34 +0530

    Time: 45 ms (5.09%) | Memory: 19.6 MB (9.47%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void dfs(int i, vector<int> &visited, vector<unordered_set<int>> &adj, vector<int> &ans) {
+        visited[i] = 1;
+
+        for(int j : adj[i]) {
+            if(!visited[j]) {
+                dfs(j, visited, adj, ans);
+            }
+        }
+        ans.push_back(i);
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& nums) {
+        int v = numCourses;
+        vector<unordered_set<int>> adj(v);
+        vector<int> visited(v, 0);
+        vector<int> ans;
+
+        for(int i = 0 ; i < nums.size(); i++) {
+            adj[nums[i][1]].insert(nums[i][0]);
+        }
+
+        for(int i = 0 ; i < visited.size(); i++) {
+            if(!visited[i]) {
+                dfs(i, visited, adj, ans);
+            }
+            // cout<<i<<" -> ";
+            // for(int x: adj[i]) cout<<x<<" ";
+            // cout<<endl;
+        }
+        // reverse(ans.begin(), ans.end());
+
+        for(int i = 0; i < v ; i++) {
+            // cout<<abs
+            for(int j = i ; j < v; j++) {
+                if(adj[ans[i]].count(ans[j])) return false;
+            }
+        }
+        return true;
+
+    }
+};
