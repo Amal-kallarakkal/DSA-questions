e1a392f98a3169227d579f7537f240c61ee88e4f
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-08-23 01:40:28 +0530

    Time: 131 ms (63.98%) | Memory: 54.8 MB (68.15%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+
+    bool cycleCheck(int i, vector<int> &visited, vector<vector<int>>& graph, vector<int> &path) {
+        visited[i] = 1;
+        path[i] = 1;
+
+        for(int j: graph[i]) {
+            if(!visited[j]) {
+                if(cycleCheck(j, visited, graph, path)) return true;
+            } else if(path[j]) {
+                return true;
+            }
+        }
+        path[i] = 0;
+        return false;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int v = graph.size();
+        vector<int> ans;
+        unordered_set<int> set;
+        vector<int> visited(v, 0);
+        vector<int> path(v, 0);
+        for(int i = 0; i < v; i++) {
+            if(graph[i].size() == 0) {
+                set.insert(i);
+                ans.push_back(i);
+            }
+        }
+
+        for(int i = 0 ; i < v; i++) {
+            for(int j: graph[i]) {
+                // if(set.count(j)) {
+                    // check cycle present or not                    
+                    if(!cycleCheck(i, visited, graph, path)) {
+                        if(!set.count(i))ans.push_back(i);
+                        set.insert(i);
+                    }
+                // }
+            }
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+        
+    }
+};
