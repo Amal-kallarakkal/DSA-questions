219053a4d09d1a3d910e13403a559a576cd3fc3e
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-09-21 15:18:46 +0530

    Time: 15 ms (57.04%), Space: 13.8 MB (69.49%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void solve(int i , vector<int> &ans, int &n) {
+        if(i > n) return ;
+        ans.push_back(i);
+        if(i == n) return;
+
+        for(int j = 0; j < 10; j++ ) {
+            solve(i*10 + j, ans, n);
+        }
+    }
+    vector<int> lexicalOrder(int n) {
+        vector<int> ans;
+        for(int i = 1; i < 10; i++) {
+            solve(i, ans, n);
+        }
+        return ans;
+    }
+};
+
+static const int speedup = [](){
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
+
