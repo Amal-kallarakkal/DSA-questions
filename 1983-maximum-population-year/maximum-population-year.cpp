225a1ee700ad472e028dbd99b070d09ba757b5e8
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-09-27 16:40:35 +0530

    Time: 3 ms (68.48%) | Memory: 11.3 MB (12.22%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.cpp
+++ 1983-maximum-population-year/maximum-population-year.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
-        int n = 0, low = INT_MAX;
-        for(auto i : logs){
-            n = max(n, i[1]);
-            low = min(low, i[0]);
-        }
-        // sort(logs.begin(), logs.end());
-        
-        // cout<<low<<", "<<n<<endl;
-        vector<int> line(n + 1);
+        map<int, int> line;        
         for(auto i : logs){
             line[i[0]]++;
             line[i[1]]--;
         }
         int ans = 0;
         int maxCnt = 0, cnt = 0;
-        for(int i = low ; i < n ; i++) {
-            cnt += line[i];
+        for(auto &i: line) {
+            cnt += i.second;
             if(cnt > maxCnt) {
                 maxCnt = cnt;
-                ans = i;
-                // c'out<<ans<<", cnt:  "<<cnt<<endl;
+                ans = i.first;
             }
         }
         return ans;
