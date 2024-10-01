f5ebd23a2b800e3bd796765299b9dac7c98d9b78
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-10-02 00:29:46 +0530

    Time: 166 ms (19.18%), Space: 82.8 MB (5.42%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
-        map<int, int> mp;
-        
+        unordered_map<int, int> mp;
+        int rem;
         for(int x : arr) {
             mp[((x % k) + k) % k]++;
         }
-        int a =  arr.size();
-        int rem ;
-
-        // for(auto x : mp) {
-        //     cout<<x.first<<" : "<<x.second<<endl;
-        // }
+       
         for(int x : arr) {
-            // cout<<x.first<<" : "<<x.second<<endl;
             rem = x%k;
-            // cout<<rem<<" and "<<k - rem<<endl;
             if(rem == 0) {
                 if(mp[rem]%2 == 1) return false;
             } 
             else if(mp[rem] != mp[k - rem ]) return false;
-            // else if(mp[2*rem]%2 != k) return false;
         }
         
         return true;
