425571a8e2ad3b35764adfbbbafeba31fa43b453
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-10-05 15:16:54 +0530

    Time: 100 ms (16.55%) | Memory: 20.1 MB (17.07%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -3,16 +3,15 @@ public:
     bool checkInclusion(string s1, string s2) {
         int n = s1.size();
         if(s2.size() < n) return false;
-        unordered_map<char, int> mp;
-        unordered_map<char, int> temp;
+        vector<int> mp(26, 0);
         
-        for(int i = 0 ; i < n ; i++) mp[s1[i]]++;
+        for(int i = 0 ; i < n ; i++) mp[s1[i] - 'a']++;
 
         for(int i = 0 ; i < s2.size() - n + 1; i++) {
-            temp.clear();
-            if(mp.count(s2[i])) {                
+            vector<int> temp(26, 0);
+            if(mp[s2[i] - 'a']) {                
                 for(int j = i ; j < i+ n ; j++) {
-                    temp[s2[j]]++;
+                    temp[s2[j] - 'a']++;
                 }
                 if(temp == mp) return true;
             }
@@ -22,9 +21,9 @@ public:
     }
 };
 
-// static const int speedup = []() {
-//     ios::sync_with_stdio(0);
-//     cin.tie(NULL);
-//     cout.tie(NULL);
-//     return 0;
-// }();
+static const int speedup = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
