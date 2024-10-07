8f0de69961a464cac9bbe4295e8609440800dc70
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-10-07 14:03:52 +0530

    Time: 4 ms (76.08%) | Memory: 11.4 MB (76.70%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -3,11 +3,7 @@ public:
     int minLength(string s) {
         stack<char> st;
         for(int i = 0; i < s.size(); i++) {    
-            if(st.empty()) {
-                st.push(s[i]);
-            } else if(st.top() == 'A' && s[i] == 'B') {
-                st.pop();
-            } else if(st.top() == 'C' && s[i] == 'D') {
+            if(!st.empty() && ((st.top() == 'A' && s[i] == 'B') || (st.top() == 'C' && s[i] == 'D') )) {
                 st.pop();
             } else {
                 st.push(s[i]);
