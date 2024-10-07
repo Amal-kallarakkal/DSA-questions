7c2f8479ca8f2bdac12de6af6a084d2ae2c402c0
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-10-07 13:53:10 +0530

    Time: 36 ms (5.05%) | Memory: 15 MB (5.05%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -5,13 +5,10 @@ public:
         string t, pre, suf;
         while(i < s.size()) {
             t = s.substr(i, 2);
-            // cout<<t<<endl;
             if(t == "AB" || t == "CD") {
                 pre = s.substr(0 , i);
                 suf = s.substr(i+2, s.size() - i - 2);\
-                // cout<<pre<<" + "<<suf<<endl;
                 s = pre + suf;
-                // cout<<s<<endl;
                 i = 0;
                 continue;
             }
@@ -20,3 +17,10 @@ public:
         return s.size();
     }
 };
+
+const static int speedup = []() {
+    ios::sync_with_stdio();
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
