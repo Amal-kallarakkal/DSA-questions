661a28590a8bff883418ca7c81db59b2823300c8
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-10-09 10:46:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (94.13%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> st;
+
+        for(int i = 0 ; i < s.size(); i++) {
+            if(st.empty() || s[i] == '(') st.push(s[i]);
+            else if(s[i] == ')') {
+                if(st.top() == '(') st.pop();
+                else st.push(s[i]);
+            }
+        }
+
+        return st.size();
+    }
+};
