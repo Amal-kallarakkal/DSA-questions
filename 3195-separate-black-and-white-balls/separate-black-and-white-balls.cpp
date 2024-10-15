e80fa85296bb9378f822224850abde6d36bcee2f
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-10-15 13:57:47 +0530

    Time: 11 ms (100.00%) | Memory: 15.7 MB (45.68%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -4,24 +4,18 @@ public:
     long long minimumSteps(string s) {
         
         ll cnt = 0;
-        priority_queue<int> pqmax_0;
-        priority_queue<int, vector<int>, greater<int>> pqmin_1;
-        for(int i = 0 ; i < s.size(); i++) { 
-            if(s[i] == '1') {
-                pqmin_1.push(i);
-            } else {
-                pqmax_0.push(i);
-            }
+        int j = s.size() - 1, i = 0;
+        while(i < j && j) { 
+            while(s[i] != '1' && i < s.size()) i++; 
+            
+            while(s[j] != '0' && j) j--;
+
+            if(i < j) cnt += j -i;
+            else break;
+            i++; 
+            j--;
         }
-        int top1, top0;
-        while(!pqmax_0.empty() && !pqmin_1.empty()) {
-            top0 = pqmax_0.top();
-            pqmax_0.pop();
-            top1 = pqmin_1.top();
-            pqmin_1.pop();
-            if(top0 < top1) break;
-            cnt += top0 - top1;
-        }
+        
         return cnt;
     }
 };
