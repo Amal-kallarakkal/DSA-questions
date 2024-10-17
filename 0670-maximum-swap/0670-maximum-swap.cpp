db5cf2c15fb1eb07dc61a0d9e11f65e01ab678b3
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-10-17 13:43:57 +0530

    Time: 0 ms (100%), Space: 7.5 MB (70.34%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        int maxi = 0;
+        int mini = INT_MAX, j, k;
+        vector<int> nums;
+        while(num) {
+            nums.push_back(num%10);
+            num /= 10;
+        }
+        reverse(nums.begin(), nums.end());
+
+        k = -1, j = -1;
+        for(int i = nums.size() - 1 ; i >= 0; i--) {
+            if(nums[i] > maxi) {
+                maxi = nums[i];
+                j = i;
+            } else if(nums[i] < maxi) {
+                k = i;
+            }            
+        }
+
+        if(k < j && k > -1) swap(nums[k], nums[j]);
+        for(int x: nums) num = num*10 + x;
+        if(k < j && k > -1) swap(nums[k], nums[j]);
+
+        int num2 = 0;
+        k = -1, j = -1;
+        maxi = 0;
+        for(int i = 0 ; i < nums.size(); i++) {
+            if(nums[i] < mini) {
+                mini = nums[i];
+                k = i;
+            } else if(nums[i] > mini) {
+                if(maxi <= nums[i]) {
+                    maxi = nums[i];
+                    j = i;
+                }
+            }            
+        }
+        if(k < j && j > -1) swap(nums[k], nums[j]);
+        for(int x: nums) num2 = num2*10 + x;
+        return max(num, num2);
+    }
+};
