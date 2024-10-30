9d00a396d542ca629649a0ef5f01551183708ef3
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-10-30 17:22:39 +0530

    Time: 22 ms (60.98%) | Memory: 74.5 MB (43.31%) - LeetSync

--- 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
-    int maxMoves(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        int res = 0;
-        vector<int> dp(m);
-        
-        for (int j = 1; j < n; ++j) {
-            int leftTop = 0;
-            bool found = false;
-            
-            for (int i = 0; i < m; ++i) {
-                int cur = -1;
-                int nxtLeftTop = dp[i];
-                
-                if (i - 1 >= 0 && leftTop != -1 && grid[i][j] > grid[i - 1][j - 1]) {
-                    cur = max(cur, leftTop + 1);
-                }
-                
-                if (dp[i] != -1 && grid[i][j] > grid[i][j - 1]) {
-                    cur = max(cur, dp[i] + 1);
-                }
-                
-                if (i + 1 < m && dp[i + 1] != -1 && grid[i][j] > grid[i + 1][j - 1]) {
-                    cur = max(cur, dp[i + 1] + 1);
-                }
-                
-                dp[i] = cur;
-                found = found || (dp[i] != -1);
-                leftTop = nxtLeftTop;
+    int dx[3] = {-1, 0, 1};
+    int nx, ny;
+
+    int dfs(int x, int y, vector<vector<int>>& grid, vector<vector<int>> &dp) {
+        int row = grid.size();
+        int col = grid[0].size();
+        if(dp[x][y]) return dp[x][y];
+
+        for(int i = 0 ; i < 3 ; i ++) {
+            nx = x + dx[i];
+            ny = y + 1;
+            if(nx < 0 || nx >= row || ny >= col) continue;
+            if(grid[nx][ny] > grid[x][y]) {
+                dp[x][y] = max(dp[x][y] , 1 + dfs(nx, ny, grid, dp));                
             }
-            
-            if (!found) break;
-            res = j;
         }
         
-        return res;
+        return dp[x][y];
+    }
+    int maxMoves(vector<vector<int>>& grid) {
+        int row = grid.size();
+        int col = grid[0].size();
+        int maxi = 0;
+        vector<vector<int>> dp(row, vector<int> (col, 0));
+
+        for(int i = 0; i < row; i++) {
+            dp[i][0] = dfs(i, 0, grid, dp);
+            maxi = max(maxi , dp[i][0]);
+        }
+        
+        return maxi;
     }
 };
