66f1725353151ebe4daeb2b4fae5df2b51ed5104
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-11-11 15:14:04 +0530

    Time: 0 ms (100.00%) | Memory: 26.7 MB (87.22%) - LeetSync

--- 2716-prime-subtraction-operation/prime-subtraction-operation.cpp
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.cpp
@@ -10,7 +10,6 @@ public:
                 if(temp%j == 0) break;
                 j++;
             }
-            // cout<<temp<<" : "<<j*j<<endl;
             if(j*j > temp) return temp;
         }
         return temp;
@@ -21,8 +20,6 @@ public:
             if(nums[i-1] >= nums[i]) nums[i-1] -= nextPrime(nums[i-1] - nums[i]);
             if(nums[i-1] <= 0) return false;
         }
-        // for(int i : nums) cout<<i <<" " ;
-
         return true;
     }
 };
