9e35e7ecf75a5d513beaa57c8d55a439e02f6419
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-11-11 15:11:21 +0530

    Time: 0 ms (100.00%) | Memory: 26.7 MB (97.12%) - LeetSync

---
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int nextPrime(int n) {
+        if(n == 0) return 2;
+        int temp, j;
+        for(int i = 1 ; i < 10; i++) {
+            temp = n + i;
+            j = 2;
+            while(j*j <= temp) {
+                if(temp%j == 0) break;
+                j++;
+            }
+            // cout<<temp<<" : "<<j*j<<endl;
+            if(j*j > temp) return temp;
+        }
+        return temp;
+    }
+    bool primeSubOperation(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = n-1 ; i > 0; i--) {
+            if(nums[i-1] >= nums[i]) nums[i-1] -= nextPrime(nums[i-1] - nums[i]);
+            if(nums[i-1] <= 0) return false;
+        }
+        // for(int i : nums) cout<<i <<" " ;
+
+        return true;
+    }
+};
