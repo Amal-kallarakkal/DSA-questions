6632eb7d1457b2409288d461290b55b71d9c9dde
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-11-13 22:47:18 +0530

    Time: 61 ms (64.88%) | Memory: 60.5 MB (18.39%) - LeetSync

---
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -0,0 +1,64 @@
+using ll = long long;
+class Solution {
+public:
+
+    int binSrchLeft(int l, int h, vector<int>& nums, int &target, int &curr) {
+        int n = nums.size(), sum, ans = -1, mid;
+        while(l <= h && l < n && h >=0) {
+            mid = (l + h)/2;
+            sum = curr + nums[mid];
+            if(sum >= target) {
+                // cout<<sum<<" >= "<<target<<" , mid:" <<mid<<endl;
+                h = mid - 1;
+                ans = mid;
+            }
+            else l = mid + 1;
+        }
+        return ans;
+    }
+
+    int binSrchRight(int l, int h, vector<int>& nums, int &target, int &curr) {
+        int n = nums.size(), sum, ans = -1, mid;
+        while(l <= h && l < n && h >=0) {
+            mid = (l + h)/2;
+            sum = curr + nums[mid];            
+            if(sum <= target) {
+                l = mid + 1;
+                ans = mid;
+            }
+            else h = mid - 1;
+        }
+        return ans;
+    }
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        ll cnt = 0;
+        int n = nums.size(), sum, l, h, left, right, mid;
+        sort(nums.begin(), nums.end());
+        // for(int i = 0 ; i < n ; i++) cout<<i<<" ";
+        // cout<<endl;
+        // for(int i : nums) cout<<i<<" ";
+        // cout<<endl;
+
+        for(int i = 0 ; i < n - 1 ; i++) {
+            l = i + 1;
+            h = n-1;
+            while(l <= h && l < n && h >=0) {
+                mid = (l + h)/2;
+                sum = nums[mid] + nums[i];
+                if(sum >= lower && sum <= upper) break;
+                else if(sum > upper) h = mid - 1;
+                else l = mid + 1;
+            }
+            if(sum < lower || sum > upper) continue;
+            // cnt++;
+            left = binSrchLeft(i+1, mid, nums, lower, nums[i]);
+            right = binSrchRight(mid, n-1, nums, upper, nums[i]);
+            // cout<<nums[i]<<" , left = "<<left<<", right = "<<right<<endl;
+            if(left < right) cnt += right - left + 1;
+            else cnt++;
+
+        }
+        return cnt;
+
+    }
+};
