4fc7babcf0aee6524ebe95c6c4346ff70f2d6c5b
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-11-14 15:35:02 +0530

    Time: 101 ms (28.46%) | Memory: 88.5 MB (15.95%) - LeetSync

--- 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -4,24 +4,19 @@ public:
 
     bool Possible(int &n, vector<int> &nums) {
         int sum = 0;
-        for(int i = 0 ; i < nums.size(); i++) {
-            sum += ceil((nums[i]*1.0)/mid);
-        }
+        for(int i = 0 ; i < nums.size(); i++) sum += ceil((nums[i]*1.0)/mid);
         if(sum <= n) return true;
         return false;
     }
-    int BinSrch(int l, int h, vector<int> &nums, int &n) {
-        if(l >= h) return h;
-
-        mid = (l + h)/2;
-
-        if(Possible(n, nums)) return BinSrch(l, mid, nums, n);
-        else return BinSrch(mid + 1, h, nums, n);
-
-        return -1;
-    }
+    
     int minimizedMaximum(int n, vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        return BinSrch(1, nums[nums.size() - 1], nums, n);
+        int l = 1, m = nums.size(), h = nums[m-1];
+        while(l < h) {
+            mid = (l + h)/2;
+            if(Possible(n, nums)) h = mid;
+            else l = mid + 1;
+        }
+        return l;
     }
 };
