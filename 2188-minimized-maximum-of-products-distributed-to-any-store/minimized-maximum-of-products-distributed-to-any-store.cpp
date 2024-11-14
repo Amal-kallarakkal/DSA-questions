b1a1cc6f3187954cc935ba66bcbcd6acf7054314
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-11-14 15:22:23 +0530

    Time: 94 ms (29.21%) | Memory: 88.4 MB (20.27%) - LeetSync

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int mid;
+
+    bool Possible(int mid,int &n, vector<int> &nums) {
+        int sum = 0;
+        for(int i = 0 ; i < nums.size(); i++) {
+            sum += ceil((nums[i]*1.0)/mid);
+        }
+        if(sum <= n) return true;
+        return false;
+    }
+    int BinSrch(int l, int h, vector<int> &nums, int &n) {
+        if(l >= h) return l;
+
+        mid = (l + h)/2;
+
+        if(Possible(mid, n, nums)) {
+            return BinSrch(l, mid, nums, n);
+        } else {
+            return BinSrch(mid + 1, h, nums, n);
+        }
+
+        return -1;
+    }
+    int minimizedMaximum(int n, vector<int>& nums) {
+        int m = nums.size();
+        sort(nums.begin(), nums.end());
+
+        if(n == m) return nums[m - 1];
+        return BinSrch(1, nums[m-1], nums, n);
+    }
+};
