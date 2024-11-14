6da605083460f921872292a90c168eeeab1ba8a4
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-11-14 15:38:47 +0530

    Time: 71 ms (34.08%) | Memory: 87.4 MB (22.06%) - LeetSync

--- 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    int mid;
-    bool Possible(int &n, vector<int> &nums) {
+    bool Possible(int &n, vector<int> &nums, int &mid) {
         int sum = 0;
         for(int i = 0 ; i < nums.size(); i++) sum += ceil((nums[i]*1.0)/mid);
         if(sum <= n) return true;
@@ -9,12 +8,11 @@ public:
     }
     
     int minimizedMaximum(int n, vector<int>& nums) {
-        // sort(nums.begin(), nums.end());
-        int l = 1, m = nums.size(), h = nums[m-1];
-        for(int i : nums) h = max(h, i);
+        int l = 1, h = 0, mid;
+        for(int &i : nums) h = max(h, i);
         while(l < h) {
             mid = (l + h)/2;
-            if(Possible(n, nums)) h = mid;
+            if(Possible(n, nums, mid)) h = mid;
             else l = mid + 1;
         }
         return l;
