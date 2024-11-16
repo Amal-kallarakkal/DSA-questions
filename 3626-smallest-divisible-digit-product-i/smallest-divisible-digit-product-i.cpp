50dcb755026d1333e9191f74abb1a971e89340e1
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-11-16 20:43:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (82.35%) - LeetSync

---
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        int temp = n, prod = 1;
+
+        for(int i = 0 ; i < 100; i++) {
+            temp = n + i, prod = 1;
+            while(temp) {
+                prod *= temp%10;
+                temp /= 10;                
+            }
+            if(prod%t == 0) return n + i;
+        }
+        return -1;
+    }
+};
