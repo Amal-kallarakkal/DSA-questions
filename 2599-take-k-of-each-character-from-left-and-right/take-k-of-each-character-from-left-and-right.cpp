25ba5f0ec1c26e8a74bcbe85e54eca5988c881ce
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-11-21 00:05:36 +0530

    Time: 87 ms (19.02%) | Memory: 13.6 MB (16.15%) - LeetSync

--- 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
+++ 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
@@ -7,21 +7,19 @@ public:
         for(int i = 0; i < n; i++) {
             mp[s[i]]++;
         }
+
         if(!(mp['a'] >= k && mp['b'] >= k && mp['c'] >= k)) return -1;
+
         while(l < n && r < n) {
-            // r = l;
-            if(r < l) r = l;
+            // if(r < l) r = l;
             while(r < n &&( mp['a'] >= k && mp['b'] >= k && mp['c'] >= k)) {
                 mp[s[r]]--;
                 if(mp['a'] >= k && mp['b'] >= k && mp['c'] >= k) maxi = max(maxi, r - l + 1);
                 r++;
             }
-            // cout<<"l: "<<l<<", r: "<<r<<endl;
-            // cout<<maxi<<endl;
-            // maxi = max(maxi, r - l + 1);
+            
             mp[s[l++]]++;
         }
-        // cout<<maxi<<endl;
         ans = n - maxi;
 
         return ans;
