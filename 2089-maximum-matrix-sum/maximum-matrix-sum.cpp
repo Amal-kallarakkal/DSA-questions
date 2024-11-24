c7ca38409f3d226aead37f3d4cda51c4fc72c708
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-11-24 13:38:48 +0530

    Time: 0 ms (100.00%) | Memory: 38.4 MB (30.74%) - LeetSync

---
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
@@ -0,0 +1,22 @@
+using ll = long long;
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        ll sum = 0, n = matrix.size(), nsum = 0, neg = 0;
+        int mini = INT_MAX;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0 ; j < n; j++) {
+                if(matrix[i][j] < 0) {
+                    neg++;
+                   sum -= matrix[i][j];
+                }
+                else sum += matrix[i][j];
+                mini= min(abs(matrix[i][j]), mini);
+            }
+        }
+        if(neg %2) {
+            sum -= 2*mini;
+        } 
+        return sum;
+    }
+};
