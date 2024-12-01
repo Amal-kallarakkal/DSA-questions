d26f4e9664777c0bf53d257060776c9c7750377f
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-12-01 15:32:53 +0530

    Time: 3 ms (24.95%) | Memory: 14.9 MB (10.47%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -4,12 +4,7 @@ public:
         unordered_map<int, int> set;
         for(int i : arr) set[i]++;
         if(set[0] >= 2) return true;
-        for(int i : arr) {
-            if(i != 0 && set.count(2*i)) {
-
-                return true;
-            }
-        }
+        for(int i : arr) if(i != 0 && set.count(2*i)) return true;
         return false;
     }
 };
