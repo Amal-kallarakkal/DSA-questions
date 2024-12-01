80e0db823c366fed17b399da9e75ff801d29bc54
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-12-01 15:32:14 +0530

    Time: 11 ms (7.25%) | Memory: 14.8 MB (10.47%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_map<int, int> set;
+        for(int i : arr) set[i]++;
+        if(set[0] >= 2) return true;
+        for(int i : arr) {
+            if(i != 0 && set.count(2*i)) {
+
+                return true;
+            }
+        }
+        return false;
+    }
+};
