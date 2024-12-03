091459f4edbad98cf4948ffa9b2a127e691facdd
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-12-03 20:15:58 +0530

    Time: 24 ms (82.91%) | Memory: 52.2 MB (82.25%) - LeetSync

--- 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.cpp
+++ 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
+        int m = queries.size(), D, curr;
+        vector<int> ans(m), dist(n);
         vector<vector<int>> adj(n);
-        for (int i = 0; i < n; i++) {
-            adj[i].push_back(i + 1);
-        }
-        
-        auto shortest_path = [&]() {
-            queue<pair<int, int>> q;
-            q.push({0, 0}); // node, length
-            unordered_set<int> visit;
-            visit.insert(0);
-            
-            while (!q.empty()) {
-                auto [cur, length] = q.front();
-                q.pop();
-                
-                if (cur == n - 1) return length;
-                
-                for (int nei : adj[cur]) {
-                    if (visit.find(nei) == visit.end()) {
-                        q.push({nei, length + 1});
-                        visit.insert(nei);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        for(int i = 0; i < n - 1; i++) adj[i].push_back(i+1);
+        for(int i = 0 ; i < n; i++) dist[i] = i;
+        for(int i = 0 ; i < m; i++) {
+            // dist, node
+            adj[queries[i][0]].push_back(queries[i][1]);
+            pq.push({dist[queries[i][0]], queries[i][0]});
+
+            while(!pq.empty()) {
+                curr = pq.top().second, D = pq.top().first;
+                pq.pop();
+                for(int x : adj[curr]) {
+                    if(dist[x] > D + 1) {
+                        dist[x] = D + 1;
+                        pq.push({dist[x], x});
                     }
                 }
             }
-            return -1;
-        };
-        
-        vector<int> res;
-        for (const auto& query : queries) {
-            adj[query[0]].push_back(query[1]);
-            res.push_back(shortest_path());
+            ans[i] = dist[n-1];
+
         }
-        return res;
+
+        return ans;
     }
 };
