69e35b0ab7a9dd9c40724c3b1934b87c949bdae3
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-12-09 16:22:11 +0530

    Time: 143 ms (33.62%) | Memory: 138.1 MB (52.12%) - LeetSync

---
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maxTwoEvents(vector<vector<int>>& events) {
+        int n = events.size(), maxi = 0, ans = 0, sum, target, l, h, mid, curr;
+        map<int, int> mp;
+        sort(events.begin(), events.end());
+
+        for(int i = n - 1 ; i >= 0; i--) {
+            maxi = max(maxi, events[i][2]);
+            mp[events[i][0]] = maxi;
+        }
+
+        for(int i = 0 ; i < n ; i++) {
+            sum = events[i][2];
+            target = events[i][1];
+            l = i + 1;
+            h = n - 1;
+            curr = -1;
+            while(l <= h) {
+                mid = (l + h)/2;
+                if(target < events[mid][0]) {
+                    curr = events[mid][0];
+                    h = mid - 1;
+                } else l = mid + 1;
+            }
+            if(curr != -1) {
+                sum += mp[curr];
+            }
+            ans = max(sum , ans);
+        }
+        return ans;
+    }
+};
