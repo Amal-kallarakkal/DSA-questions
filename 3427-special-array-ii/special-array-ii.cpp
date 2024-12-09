cfe98adca51e2f4c2d1b7f06eeff28f8ac6c64f7
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-12-09 15:05:08 +0530

    Time: 24 ms (58.59%) | Memory: 126.4 MB (68.49%) - LeetSync

--- 3427-special-array-ii/special-array-ii.cpp
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -36,22 +36,20 @@ public:
 
     }
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        ios::sync_with_stdio(0);
+        cin.tie(NULL);
+        cout.tie(NULL);
         int n = queries.size(), sum = 0, left, right;
         vector<bool> ans(n);
         vector<int> flag;
-        string temp;
         for(int i = 0 ; i < nums.size() - 1; i++) {
             sum = nums[i] + nums[i+1];
-            // cout<<sum<<endl;
             if(sum% 2 == 0) flag.push_back(i+1);
         }
-        // for(int i : flag) cout<<i<<" ";
-        // cout<<endl;
+        
         for(int i = 0 ; i < n; i++) {
-            //
             left = leftbs(queries[i][0], flag);
             right = rightbs(queries[i][1], flag);
-            // cout<<queries[i][0]<<", "<<queries[i][1]<<" : "<<left<<" , "<<right<<endl;
             if(left > right || left == -1 || right == -1) ans[i] = true;
         }
 
