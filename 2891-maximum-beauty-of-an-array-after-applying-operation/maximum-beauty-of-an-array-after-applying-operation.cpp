39580e924cb499e1b36f27c7b5ef3b46223b5ed4
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2024-12-11 16:42:45 +0530

    Time: 58 ms (51.66%) | Memory: 104.7 MB (97.93%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
+        ios::sync_with_stdio(0);
         int n = nums.size(), l, h, mid, ans = 0;
         sort(nums.begin(), nums.end());
         for(int i = 0 ; i < n; i++) {
