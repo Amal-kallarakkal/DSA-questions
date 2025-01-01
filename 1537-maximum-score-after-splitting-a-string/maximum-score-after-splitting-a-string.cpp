ab3ab0512688e3c07262a9a2b9c0686964c9bf5c
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-01-01 17:43:19 +0530

    Time: 45 ms (5.34%) | Memory: 9.4 MB (11.67%) - LeetSync

---
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxScore(string s) {
+        int ans, zcnt = 0, ocnt = 0;
+        vector<int> one(s.size(), 0), zero(s.size(), 0);
+        for(int i = 0, j; i < s.size(); i++) {
+            
+            if(s[i] == '0') {
+                zcnt++;
+                zero[i] = zcnt;
+            } 
+            j = s.size() - 1 - i;
+            if(s[j] == '1') {
+                ocnt++;
+                one[j] = ocnt;
+            }
+        }
+        for(int i = 0 ; i < s.size() - 1; i++) {
+            ans = max(ans, one[i + 1] + zero[i]);
+            cout<<one[i]<<" , "<<zero[i]<<endl;
+        }
+
+        return ans;
+
+    }
+};
