6011df12213069aae4ccad8ee8f82e4621a672b1
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-01-07 22:13:27 +0530

    Time: 11 ms (10.89%) | Memory: 11.7 MB (29.52%) - LeetSync

---
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string> ans;
+        unordered_set<string> set;
+        int n = words.size();
+        string temp;
+
+        for(int i = 0 ; i < n; i++) {
+            for(int j = 0 ; j < n; j++) {
+                if(j == i) continue;
+                if(words[i].size() > words[j].size()) continue;
+                else if(words[i].size() == words[j].size() && words[j] == words[i]) {
+                    set.insert(words[i]);
+                    break;
+                } 
+
+                for(int k = 0; k < words[j].size() - words[i].size() + 1; k++) {
+                    temp = words[j].substr(k, words[i].size());
+                    if(temp == words[i]) {
+                        set.insert(words[i]);
+                        break;
+                    }
+                }
+            }
+        }
+        for(string x : set) ans.push_back(x);
+        
+        return ans;
+    }
+};
