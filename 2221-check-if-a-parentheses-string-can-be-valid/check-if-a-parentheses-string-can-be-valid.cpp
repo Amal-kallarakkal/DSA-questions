cee367745e8eaef0a2c9e5ad6afdb0b92e5f6d57
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-01-12 23:18:35 +0530

    Time: 14 ms (55.08%) | Memory: 30.1 MB (53.05%) - LeetSync

---
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        int opencnt = 0, closecnt = 0, wildcnt = 0, n = s.size();
+        if(n&1) return false;
+        for(int i = 0; i < n; i++) {
+            if(locked[i] == '1') {
+                if(s[i] == ')') closecnt++;
+                else opencnt++;
+            } else wildcnt++;
+            if(closecnt > opencnt + wildcnt) return false;
+        }
+        cout<<"reverse pass"<<endl;
+        opencnt = 0, closecnt = 0, wildcnt = 0;
+        for(int i = n-1; i >= 0; i--) {
+            if(locked[i] == '1') {
+                if(s[i] == ')') closecnt++;
+                else opencnt++;
+            } else wildcnt++;
+            if(opencnt > closecnt + wildcnt) return false;
+        }
+        return true;
+    }
+};
