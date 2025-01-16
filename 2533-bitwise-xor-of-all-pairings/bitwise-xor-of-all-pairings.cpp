7af8be85d02fcda3ee3552284586f9838a828399
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-01-17 01:38:41 +0530

    Time: 0 ms (100.00%) | Memory: 64.2 MB (83.40%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
@@ -7,7 +7,6 @@ public:
         for(int i = 0; i < n2; i++) ans2 ^= nums2[i];
         if(n1%2 == 0) ans2 = 0;
         if(n2%2 == 0) ans1 = 0;
-        cout<<ans1<<", "<<ans2<<endl;
         ans = ans1^ans2;
         return ans;
     }
