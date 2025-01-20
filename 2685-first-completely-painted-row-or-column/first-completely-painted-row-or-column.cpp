d3df2ac49dc5edfaf887832c17a8d60e8678d439
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-01-20 20:14:43 +0530

    Time: 79 ms (80.18%) | Memory: 161.8 MB (68.60%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -3,7 +3,7 @@ public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
 
         int ans = 0, row = mat.size(), col = mat[0].size();
-        unordered_map<int, pair<int, int>> mp;
+        vector<vector<int>> mp(arr.size() + 1);
         vector<int> rowarr(row), colarr(col);
         for(int i = 0; i < row; i++) {
             for(int j = 0 ; j < col; j++) {
@@ -11,10 +11,10 @@ public:
             }
         }
         for(int i = 0; i < arr.size(); i++) {
-            colarr[mp[arr[i]].second]++;
-            rowarr[mp[arr[i]].first]++;
-            if(colarr[mp[arr[i]].second] == row) return i;
-            if(rowarr[mp[arr[i]].first] == col) return i;
+            rowarr[mp[arr[i]][0]]++;
+            colarr[mp[arr[i]][1]]++;
+            if(colarr[mp[arr[i]][1]] == row) return i;
+            if(rowarr[mp[arr[i]][0]] == col) return i;
         }
         return ans;
     }
