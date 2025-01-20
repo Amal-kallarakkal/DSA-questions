d2f84a9284de0e53e7afed5b40e05e3ea4663ba7
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-01-20 19:58:44 +0530

    Time: 100 ms (55.79%) | Memory: 170.4 MB (56.71%) - LeetSync

---
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        int ans = 0, row = mat.size(), col = mat[0].size(), sum;
+        unordered_map<int, pair<int, int>> mp;
+        vector<int> rowarr(row), colarr(col);
+        for(int i = 0; i < row; i++) {
+            for(int j = 0 ; j < col; j++) {
+                mp[mat[i][j]] = {i, j};
+            }
+        }
+        for(int i = 0; i < arr.size(); i++) {
+            colarr[mp[arr[i]].second]++;
+            rowarr[mp[arr[i]].first]++;
+            if(colarr[mp[arr[i]].second] == row) return i;
+            if(rowarr[mp[arr[i]].first] == col) return i;
+        }
+        return ans;
+    }
+};
