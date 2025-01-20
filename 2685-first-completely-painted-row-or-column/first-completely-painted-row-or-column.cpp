8cad12f64158e87ff141549eb8a2f9386d77a7bb
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-01-20 20:01:12 +0530

    Time: 110 ms (37.19%) | Memory: 170.4 MB (56.71%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
-        int ans = 0, row = mat.size(), col = mat[0].size(), sum;
+        ios::sync_with_stdio(0);
+        cin.tie(NULL);
+        cout.tie(NULL);
+
+        int ans = 0, row = mat.size(), col = mat[0].size();
         unordered_map<int, pair<int, int>> mp;
         vector<int> rowarr(row), colarr(col);
         for(int i = 0; i < row; i++) {
