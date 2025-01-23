de82628272aebfad6affba891ddb495c95cb1264
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-01-23 11:38:34 +0530

    Time: 0 ms (100%), Space: 26.7 MB (39.9%) - LeetHub

---
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countServers(vector<vector<int>>& grid) {
+        int row = grid.size(), col = grid[0].size(), alone = 0, total = 0, cnt;
+        vector<int> RowSum(row), ColSum(col);
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                if(grid[i][j]) total++;
+            }
+        }
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                if(grid[i][j]) RowSum[i]++;
+            }
+        }
+
+        for(int i = 0; i < col; i++) {
+            for(int j = 0; j < row; j++) {
+                if(grid[j][i]) ColSum[i]++;
+            }
+        }
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                if(grid[i][j] && (RowSum[i] == 1 && ColSum[j] == 1)) total--;
+            }
+        }
+        return total;
+    }
+};
