52ed8af2bdae7b2b014b67f512a1174d9509684c
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-02-07 21:23:20 +0530

    Time: 309 ms (67.80%) | Memory: 85 MB (45.82%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
+    int nC2(int n) {
+        if(n <= 2) return n;
+        return n*(n-1);
+    }
     int tupleSameProduct(vector<int>& nums) {
-        int n = nums.size();
-        unordered_map<int, int> hm;
-        hm.reserve(n * (n - 1) / 2); // Pre-allocate space to reduce rehashing
-
-        // Step 1: Compute products of all pairs and store their frequency
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                int mul = nums[i] * nums[j];
-                hm[mul]++;
+        int cnt = 0, n = nums.size();
+        unordered_map<int, int> mp;
+        for(int i = 0 ; i < n-1; i++) {
+            for(int j = i+1 ; j < n; j++) mp[nums[i]*nums[j]]++;
+        }
+        for(auto x : mp) {
+            if(x.second > 1) {
+                cnt += nC2(x.second); 
             }
         }
-        
-        int total = 0;
-        // Step 2: Count valid tuples based on frequency
-        for (const auto &it: hm) {
-            int count = it.second;
-            if (count >= 2)
-                total += count * (count - 1) / 2;  // Choose 2 pairs from count pairs
-        }
-        
-        return total * 8;  // Each tuple (a, b, | c, d) can be arranged in 8 ways
+        return cnt*4;
     }
 };
