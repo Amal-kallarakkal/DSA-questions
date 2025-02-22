946eb2fd47a23403eba1e18ebb27f97a1f1ced7f
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-02-22 17:10:44 +0530

    Time: 7 ms (82.28%), Space: 34.7 MB (27.8%) - LeetHub

--- 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.cpp
+++ 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.cpp
@@ -1,27 +1,35 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class FindElements {
-    unordered_set<int> recoveredValues;
-
-    void recoverTree(TreeNode* root) {
-        if (!root) return;
-        recoveredValues.insert(root->val);
-        if (root->left) {
-            root->left->val = 2 * root->val + 1;
-            recoverTree(root->left);
-        }
-        if (root->right) {
-            root->right->val = 2 * root->val + 2;
-            recoverTree(root->right);
-        }
-    }
-
 public:
-    FindElements(TreeNode* root) {
-        root->val = 0;
-        recoverTree(root);
+    unordered_set<int> set;
+    void solve(int x, TreeNode * root) {
+        if(!root) return;
+        root->val =  x;
+        set.insert(x);
+        if(root->left) solve(x*2 + 1, root->left);
+        if(root->right) solve(x*2 + 2, root->right);
     }
-
+    FindElements(TreeNode* root) {
+        solve(0, root);
+    }
+    
     bool find(int target) {
-        return recoveredValues.count(target);
+        return (set.count(target));
     }
 };
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements* obj = new FindElements(root);
+ * bool param_1 = obj->find(target);
+ */
