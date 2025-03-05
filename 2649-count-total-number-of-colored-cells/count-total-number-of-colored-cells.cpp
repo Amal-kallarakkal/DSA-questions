0f8716090dde6bb3526d00fb9324b079533b81a3
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-03-06 00:46:46 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (7.10%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -3,9 +3,8 @@ class Solution {
 public:
     long long coloredCells(int n) {
         ll ans = 1;
-        for(int i = 1 ; i < n; i++) {
-            ans += i*4;
-        }
+        ll m = n;
+        ans = 2*m*m - 2*m + 1;
         return ans;
     }
 };
