7977e6aa9622be60ef24386aa98b9987d2992ecd
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-03-24 22:35:40 +0530

    Time: 71 ms (53.06%) | Memory: 126.6 MB (43.20%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -2,22 +2,18 @@ using ll = long long;
 class Solution {
 public:
     int countDays(int n, vector<vector<int>>& nums) {
-        int ans = 0, s, e, j, store;
-        vector<vector<int>> nums2;
+        int ans = 0, j, maxi;
         sort(nums.begin(), nums.end());
-        // for(int i = 0 ; i < nums.size(); i++) {
-        //     cout<<"["<<nums[i][0]<<", "<<nums[i][1]<<"], ";
-        // }cout<<endl;
+        
         for(int i = 0; i < nums.size(); i++) {
             j = i;
-            store = nums[i][1];
-            while(j < nums.size() && store >= nums[j][0]) {
-                store = max(store, nums[j][1]);
+            maxi = nums[i][1];
+            while(j < nums.size() && maxi >= nums[j][0]) {
+                maxi = max(maxi, nums[j][1]);
                 j++;
                 if(j >= nums.size()) break;
             } 
-            ans += (store - nums[i][0]) + 1;
-            // cout<<j<<", ans: "<<store<<", "<<nums[i][0]<<endl;
+            ans += (maxi - nums[i][0]) + 1;
             i = j - 1;
         }
         
