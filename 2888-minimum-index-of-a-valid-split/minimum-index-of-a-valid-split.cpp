7a1d693df14c34e3c0645896044251c4222f8487
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-03-28 01:01:02 +0530

    Time: 35 ms (53.69%) | Memory: 98.6 MB (47.95%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -13,10 +13,14 @@ public:
                 dom = x.first;      //dominant element
             }
         }
+        
+        if(maxi*2 < n) return -1;
 
         for(int i = 0; i < n; i++) {
             if(nums[i] == dom) cnt++;         //count the freq of dom before partition
+
             if(cnt*2 > i + 1) {             //check if first half condition is satisfied
+
                 if((maxi - cnt)*2 > n - i - 1)  //check if second half condition is satisfied
                     return i;
             }
