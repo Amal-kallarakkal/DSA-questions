eb477e69f322369e16b835ab97d75104d180267a
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-03-28 00:59:11 +0530

    Time: 38 ms (50.41%) | Memory: 98.6 MB (56.56%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -2,29 +2,27 @@ class Solution {
 public:
     int minimumIndex(vector<int>& nums) {
         unordered_map<int, int> mp;
-        priority_queue<pair<int, int>> pq;
+        int maxi = 0, n = nums.size(), dom, cnt = 0;
 
         for(int i = 0 ; i < nums.size(); i++) {
             mp[nums[i]]++;
         }
         for(auto x: mp) {
-            pq.push({x.second, x.first});
+            if(maxi < x.second) {
+                maxi = x.second;    //freq of dominant element
+                dom = x.first;      //dominant element
+            }
         }
 
-        int top = pq.top().first, n = nums.size(), x, cnt = 0;
-
-        // while(!pq.empty() && top*2 > n) {            
-            x = pq.top().second;
-            cnt = 0;
-            for(int i = 0; i < n; i++) {
-                if(nums[i] == x) cnt++;
-                if(cnt*2 > i + 1) {
-                    if((top - cnt)*2 > n - i - 1) return i;
-                }
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == dom) cnt++;         //count the freq of dom before partition
+            if(cnt*2 > i + 1) {             //check if first half condition is satisfied
+                if((maxi - cnt)*2 > n - i - 1)  //check if second half condition is satisfied
+                    return i;
             }
-            // pq.pop();
-            // top = pq.top().first;
-        // }
+        }
+
+            
         return -1;
     }
 };
