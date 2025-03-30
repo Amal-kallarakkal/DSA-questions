4fbce00633dab6797a5938fdbaeef7a8893179e9
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-03-31 00:53:26 +0530

    Time: 4 ms (14.38%) | Memory: 9.5 MB (7.16%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -11,25 +11,18 @@ public:
         }
         int i = 0; 
         for(auto x: mp) {
-            // cout<<x.first<<": "<<x.second.first<<", "<<x.second.second<<endl;
             nums.push_back({x.second.first, x.second.second});
         }
+
         sort(nums.begin(), nums.end());
 
         for(int i = 0; i < nums.size(); i++) {
-            cout<<nums[i][0]<<", "<<nums[i][1]<<endl;
-            if(maxi < nums[i][0]) {
-                // cout<<"nums > maxi "<<nums[i][0]<<endl;
-                // if(ans.size() == 0) ans.push_back(maxi);
-                 ans.push_back(nums[i][0]);
-            }
+            if(maxi < nums[i][0]) ans.push_back(nums[i][0]);
             maxi = max(maxi, max(nums[i][0], nums[i][1]));
         }
 
         ans.push_back(maxi);
-        cout<<endl;
         for(int i = 1; i < ans.size(); i++) {
-            cout<<ans[i]<<", ";
             sum += ans[i-1];
             ans[i] -= sum;
         }
