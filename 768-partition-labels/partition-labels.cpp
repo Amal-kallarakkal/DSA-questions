6f866c4aa95fa510e67a489aa5ed1160a024c1dc
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-03-31 00:58:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (71.56%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        map<char, pair<int, int>> mp;
+        unordered_map<char,int> mp;
         int n = s.size(), maxi = 0, sum = 0;
         vector<int> ans;
-        vector<vector<int>> nums;
         for(int i = 0 ; i < n ; i++) {
-            if(!mp.count(s[i])) mp[s[i]].first = i;
-            else mp[s[i]].second = i;
+            mp[s[i]] = i;
         }
-        int i = 0; 
-        for(auto x: mp) {
-            nums.push_back({x.second.first, x.second.second});
-        }
-
-        sort(nums.begin(), nums.end());
-
-        for(int i = 0; i < nums.size(); i++) {
-            if(maxi < nums[i][0]) ans.push_back(nums[i][0]);
-            maxi = max(maxi, max(nums[i][0], nums[i][1]));
+       
+        for(int i = 0; i < n; i++) {
+            if(maxi < i) ans.push_back(i);
+            maxi = max(maxi, mp[s[i]]);
         }
 
         ans.push_back(maxi);
