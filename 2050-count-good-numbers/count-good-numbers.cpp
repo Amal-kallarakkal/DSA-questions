118c92579e191f2d8757b731a078ad72e14b0ee8
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-04-14 12:39:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (34.98%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -4,8 +4,8 @@ public:
     int Pow(long long x, long long n) {
         if(n == 0) return 1;
         if(n == 1) return x;
-        if(n%2) return (x * Pow(((x)%m * (x)%m)%m, n/2))%m;
-        return Pow(((x)%m * (x)%m)%m, n/2)%m;
+        if(n%2) return (x * Pow(x%m * x%m, n/2))%m;
+        return Pow(x%m * x%m, n/2)%m;
     }
     int countGoodNumbers(long long n) {                
         return (1LL * Pow(5,n - n/2) % m * Pow(4, n/2) % m) % m;
