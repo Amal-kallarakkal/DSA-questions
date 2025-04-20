5ac2ffccbaaf05ed37845dbebe5c7746ccf7ab53
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-04-20 13:55:32 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (42.46%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.cpp
+++ 797-rabbits-in-forest/rabbits-in-forest.cpp
@@ -3,13 +3,11 @@ public:
     int numRabbits(vector<int>& nums) {
         int ans = 0, n = nums.size();
         unordered_map<int, int> mp;
-        for(int i = 0 ; i < n; i++) {
-            if(nums[i]) mp[nums[i]]++;
-            else ans++;
-        }
-        for(auto i : mp) {
-            ans += (i.first + 1)*(ceil(i.second*1.0/(i.first + 1)));
-        }
+
+        for(int i = 0 ; i < n; i++) mp[nums[i]]++;
+
+        for(auto i : mp) ans += (i.first + 1)*(ceil(i.second*1.0/(i.first + 1)));
+
         return ans;
     }
 };
