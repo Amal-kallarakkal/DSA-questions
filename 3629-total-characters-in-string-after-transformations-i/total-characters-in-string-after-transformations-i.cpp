eea716cd48212babfedd9da9fe2c62f2e70ee799
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-05-13 22:36:37 +0530

    Time: 1093 ms (5.42%) | Memory: 467 MB (35.47%) - LeetSync

---
+++ 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.cpp
@@ -0,0 +1,27 @@
+using ll = long long;
+int m = pow(10, 9) + 7;
+class Solution {
+public:
+    int lengthAfterTransformations(string s, int t) {
+        vector<ll> mp1(26);
+        int sum = 0;
+        for(int i = 0 ; i < s.size(); i++) {
+            mp1[s[i] - 'a']++;
+        }
+        while(t--) {
+            vector<ll> mp2(26);
+            for(int i = 0 ;i < 26; i++) {
+                if(i < 25) mp2[i+1] = (mp2[i+1]%m + mp1[i]%m)%m;
+                else {
+                    mp2[0] = (mp2[0]%m + mp1[i]%m)%m;
+                    mp2[1] = (mp2[1]%m + mp1[i]%m)%m;
+                }
+            }
+            mp1 = mp2;
+        }
+        for(int i = 0 ; i < 26; i++) {
+            sum = (sum + mp1[i])%m;
+        }
+        return sum;
+    }
+};
