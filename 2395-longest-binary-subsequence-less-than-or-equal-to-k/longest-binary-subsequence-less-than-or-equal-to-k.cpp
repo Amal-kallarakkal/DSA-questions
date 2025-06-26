0c20c8f25016667e8eab0ae2eae678a8972815bb
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-06-27 01:14:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (50.75%) - LeetSync

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-    int f(int i, int size, int sum, string &s, vector<vector<int>>& dp) {
-        if (i < 0) return 0;
-        if (dp[i][size] != -1) return dp[i][size];
-
-        int no = f(i - 1, size, sum, s, dp);
-        int yes = 0;
-
-        if ((sum - (s[i] - '0') * pow(2, size)) >= 0) {
-            yes = 1 + f(i - 1, size + 1, sum - (s[i] - '0') * pow(2, size), s, dp);
-        }
-
-        return dp[i][size] = max(no, yes);
-    }
-
     int longestSubsequence(string s, int k) {
-        int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        return f(n - 1, 0, k, s, dp);
+        
+        int ans = 0;
+        long long limit = 0, n = s.size() - 1;
+
+        for(int i = n ; i >= 0; i--) {
+            if(s[i] == '0') ans++;
+            else {
+                if(n - i <= 60) limit += pow(2, n - i);
+                else limit = INT_MAX;
+                if(limit <= k) ans++;
+            }
+        }
+        return ans;
     }
 };
