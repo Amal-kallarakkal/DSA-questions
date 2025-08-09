54ec93c4f26fb49002d5d86e6dc32543a0357260
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-08-09 19:50:03 +0530

    Time: 0 ms (100%), Space: 8 MB (17.71%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(!n) return false;
-        while(n) {
-            // if(n == 1 || n == -1) return true;
-            if((n%2 && n > 2) || n <= 0) return false;
-            n = n/2;
-        }
+        if(n < 1 || n & (n-1)) return false;
         return true;
-    }
+    }   
 };
