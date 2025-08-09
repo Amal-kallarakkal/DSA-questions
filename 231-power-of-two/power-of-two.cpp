8d4080b98af0a4831a9fa7da26c878c7742b45f0
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-08-09 19:50:06 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (17.71%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n <= 0) return false;
-        while(n) {
-            if(n%2 && n > 2) return false;
-            n = n/2;
-        }
+        if(n < 1 || n & (n-1)) return false;
         return true;
-    }
+    }   
 };
