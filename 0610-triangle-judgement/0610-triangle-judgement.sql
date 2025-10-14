b5b88c1376293119c7c962a18e2767c5ed2e6d6f
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-10-14 09:56:12 +0530

    Time: 489 ms (11.32%), Space: 0B (100%) - LeetHub

---
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+select * , 
+    case 
+        when x + y > z and z + x > y and z + y > x then 'Yes'
+        else 'No'
+        end as triangle
+from Triangle 
