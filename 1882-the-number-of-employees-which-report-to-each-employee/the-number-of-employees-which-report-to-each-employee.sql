cbe5d77cfb3b64e3030e67d0f1cd887d0b56d016
Amal-kallarakkal <149791206+Amal-kallarakkal@users.noreply.github.com>
2025-10-14 08:37:49 +0530

    Time: 635 ms (95.74%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -1,8 +1,9 @@
 /* Write your T-SQL query statement below */
-select  e2.reports_to as employee_id,
-        max(e1.name) as name,
-        count(e2.reports_to) as reports_count,
-        round(avg(e2.age * 1.0), 0) as average_age 
-from Employees e1
-join Employees e2 on e1.employee_id = e2.reports_to
-group by e2.reports_to;
+select  mgr.employee_id,
+        mgr.name,
+        count(emp.reports_to) as reports_count,
+        round(avg(emp.age * 1.0), 0) as average_age 
+from Employees mgr
+join Employees emp on mgr.employee_id = emp.reports_to
+group by mgr.employee_id,mgr.name
+order by employee_id
